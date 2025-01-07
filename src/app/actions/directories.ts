e7d284dbc8121089bb1098fd133a2756b603d685
Andrew Ching <asching19@gmail.com>
2025-01-07 12:34:21 -0500

    code cleanup and documentation

--- src/app/actions/directories.ts
+++ src/app/actions/directories.ts
@@ -3,89 +3,111 @@ import TreeNode from '../utils/TreeNode';
 
 const root = new TreeNode('Root');
 
-function createDirectory(path: string) {
+/**
+ * Creates a directory at the specified path.
+ * 
+ * @param {string} path The path of the directory to be added.
+ * @returns {boolean} `true` if the directory was created successfully, `false` if any directory in the path doesn't exist or the end of the path is reached without createing a directory.
+ */
+function createDirectory(path: string): boolean {
     const directories = path.split('/');
     let parent = root;
 
     for (let i = 0; i < directories.length; i++) {
-        const existingChild = parent.getChild(directories[i]);
+        const currentDir = directories[i];
+        const existingChild = parent.getChild(currentDir);
 
-        if (i == directories.length - 1  && !parent.isChildOfParent(directories[i])) {
-            const child = new TreeNode(directories[i]);
-            parent.addChild(child);
+        if (i === directories.length - 1 && !parent.isChildOfParent(currentDir)) {
+            parent.addChild(new TreeNode(currentDir));
+            return true;
         }
 
         if (!existingChild) {
-            return;
+            return false;
         }
 
         parent = existingChild;
-    };
+    }
+
+    return false;
 }
 
-function moveDirectory(childPath: string, destinationPath: string) {
-    const directories = childPath.split('/');
-    let parentPath = null;
-    directories.pop();
-    parentPath = directories.join('/');
-    const parent = parentPath ? root.getChildFromPath(parentPath) : root;
+/**
+ * Moves a directory from one location to another.
+ * 
+ * @param {string} childPath The path of the directory to be moved.
+ * @param {string} destinationPath The path of the destination directory where the child will be moved.
+ * @returns {boolean} `true` if the directory was moved successfully, `false` if parent, child, or destination directories don't exist or the destination is a desendent of the directory being moved.
+ */
+function moveDirectory(childPath: string, destinationPath: string): boolean {
     const child = root.getChildFromPath(childPath);
     const destination = root.getChildFromPath(destinationPath);
+    const parentPath = childPath.slice(0, childPath.lastIndexOf('/'));
+    const parent = childPath.includes('/') ? root.getChildFromPath(parentPath) : root;
 
-    if(parent && child && destination && !root.isDescendant(destination, child)){
+    if (parent && child && destination && !root.isDescendant(destination, child)) {
         parent.moveChildTo(destination, child);
+        return true;
     }
+
+    return false;
 }
 
-function deleteDirectory(path: string) {
+/**
+ * Deletes a directory at the specified path.
+ * 
+ * @param {string} path The path of the directory to be deleted.
+ * @returns {boolean} `true` if the directory was deleted successfully, `false` if the directory or parent doesn't exist.
+ */
+function deleteDirectory(path: string): boolean {
     const child = root.getChildFromPath(path);
-    const directories = path.split('/');
-    let parentPath = null;
-    directories.pop();
-    parentPath = directories.join('/');
+    const parentPath = path.slice(0, path.lastIndexOf('/'));
     const parent = parentPath ? root.getChildFromPath(parentPath) : root;
 
-    if(child && parent) {
+    if (parent && child) {
         parent.removeChild(child);
         return true;
-    } else {
-        return false;
     }
+
+    return false;
 }
 
-export async function getOutputText(inputText: string) {
+/**
+ * Processes a list of commands and returns the output based on the actions.
+ * 
+ * @param {string} inputText The list of commands to be run
+ * @returns {string} A string containing the results of all the processed commands.
+ */
+export async function getOutputText(inputText: string): Promise<string> {
     root.clear();
-
-    const lines = inputText.split('\n');
+    const lines = inputText.split('\n').map(line => line.trim());
     let outputText = '';
 
-    lines.forEach(line => {
-        line = line.trim();
-        const command = line.split(' ');
-        const action = command[0].trim().toLocaleLowerCase();
-
+    for (const line of lines) {
+        const [action, ...args] = line.split(' ').map(arg => arg.trim().toLowerCase());
+        outputText += line + '\n';
         switch (action) {
+            // createDirectory and moveDirectory return booleans similar to deleteDirectory. This could be used to output error text.
             case 'create':
-                createDirectory(command[1]);
-                outputText += line + '\n';
+                createDirectory(args[0]);
                 break;
             case 'move':
-                moveDirectory(command[1], command[2])
-                outputText += line + '\n';
+                moveDirectory(args[0], args[1]);
                 break;
             case 'delete':
-                const isSuccess = deleteDirectory(command[1]);
-                outputText += line + '\n';
-                outputText += isSuccess ? '': 'Cannot delete ' + command[1] + ' - ' + root.getMissingDirectory(command[1]) + ' does not exist\n';
+                const isSuccess = deleteDirectory(args[0]);
+                if (!isSuccess) {
+                    outputText += 'Cannot delete ' + args[0] + ' - ' + root.getMissingDirectory(args[0]) + ' does not exist\n'
+                }
                 break;
             case 'list':
-                outputText += command + '\n';
                 outputText += root.toString();
-                break;
+                break; 
             default:
+                //Could add to outputText here to notify of unrecognized commands.
                 break;
         }
-    });
+    }
 
     return outputText;
 }
--- src/app/utils/TreeNode.ts
+++ src/app/utils/TreeNode.ts
@@ -1,3 +1,8 @@
+/**
+ * Represents a tree node data stucture
+ * Provides methods to manage and interact with the tree structure.
+ */
+
 export default class TreeNode {
     value: string;
     children: TreeNode[];
@@ -7,19 +12,51 @@ export default class TreeNode {
         this.children = [];
     }
 
+     /**
+     * Adds a child node to the current node.
+     * 
+     * @param {TreeNode} child The child node to add.
+     */
+
     addChild(child: TreeNode): void {
         this.children.push(child);
     }
+
+    /**
+     * Removes a child node from the current node.
+     * 
+     * @param {TreeNode} child The child node to remove.
+     */
 
     removeChild(child: TreeNode): void {
         this.children = this.children.filter(c => c !== child);
     }
 
-    moveChildTo(newParent: TreeNode, child: TreeNode) {
+     /**
+     * Moves a child node from the current node to a new parent node.
+     * 
+     * @param {TreeNode} newParent The new parent node to move the child to.
+     * @param {TreeNode} child The child node to move.
+     */
+    moveChildTo(newParent: TreeNode, child: TreeNode): void {
         this.removeChild(child);
         newParent.addChild(child);
     }
 
+    /**
+     * Clears all children from the current node.
+     */
+    clear(): void {
+        this.children = [];
+    }
+
+    /**
+     * Checks if a node is a descendant of the given ancestor node.
+     * 
+     * @param {TreeNode} ancestor The ancestor node to check.
+     * @param {TreeNode} node The node to check for as a descendant.
+     * @returns {boolean} `true` if the node is a descendant of the ancestor; otherwise, `false`.
+     */
     isDescendant(ancestor: TreeNode, node: TreeNode): boolean {
         if (ancestor === node) return true;
         for (const child of ancestor.children) {
@@ -31,16 +68,54 @@ export default class TreeNode {
         return false;
     }
 
-    isChildOfParent(child: string): boolean {
-        return this.children.some(c => c.value === child);
+     /**
+     * Checks if a specific child exists from the current node.
+     * 
+     * @param {string} value The value of the child node to check for.
+     * @returns {boolean} `true` if the child exists; otherwise, `false`.
+     */
+    isChildOfParent(value: string): boolean {
+        return this.children.some(c => c.value === value);
     }
 
+    /**
+     * Retrieves a child node from the current node by its value.
+     * 
+     * @param {string} value The value of the child node to find.
+     * @returns {TreeNode | null} The child node if found, otherwise `null`.
+     */
     getChild(value: string): TreeNode | null {
         const child = this.children.find(child => child.value === value);
 
         return child || null;
     }
 
+    /**
+     * Retrieves a node from a specified path.
+     * 
+     * @param {string} path The path to the node, where each directory is separated by '/'.
+     * @returns {TreeNode | null} The final node in the path if it exists, otherwise `null`.
+     */
+    getChildFromPath(path: string): TreeNode | null {
+        const directories = path.split('/');
+        let child: TreeNode | null = this;
+
+        for (const directory of directories) {
+            child = child.getChild(directory);
+            if (!child) {
+                return null;
+            }
+        }
+
+        return child;
+    }
+
+    /**
+     * Checks if a directory in the given path is missing from the current node.
+     * 
+     * @param {string} path The path to check for the missing directory.
+     * @returns {string | null} The first missing directory if found, otherwise `null`.
+     */
     getMissingDirectory(path: string): string | null {
         const directories = path.split('/');
         let currentChild: TreeNode = this;
@@ -56,20 +131,12 @@ export default class TreeNode {
         return null;
     }
 
-    getChildFromPath(path: string): TreeNode | null {
-        const directories = path.split('/');
-        let child: TreeNode | null = this;
-
-        for (const directory of directories) {
-            child = child.getChild(directory);
-            if (!child) {
-                return null;
-            }
-        }
-
-        return child;
-    }
-
+    /**
+     * Converts the tree starting from this node to a string representation.
+     * 
+     * @param {number} indentLevel The level of indentation for formatting.
+     * @returns {string} A string representing the tree structure, with child nodes indented.
+     */
     toString(indentLevel: number = 0): string {
         let result = '';
         const sortedChildren = this.children.sort((a, b) => a.value.localeCompare(b.value));
@@ -79,9 +146,5 @@ export default class TreeNode {
         });
 
         return result;
-    }
-
-    clear(): void {
-        this.children = [];
     }
 }
